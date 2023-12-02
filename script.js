05ff71b3d061fff570d41aa06a95363e3f896ba0
dangkhoa2004 <149858139+dangkhoa2004@users.noreply.github.com>
2023-12-02 23:01:44 +0700

    khoa
    update

--- script.js
+++ script.js
@@ -27,3 +27,13 @@ function redirectToContact() {
 function redirectToThongtin() {
     window.location.href = 'loadingthongtin.html';
 }
+/*SLIDE SHOW*/
+document.getElementById('next').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').appendChild(lists[0]);
+}
+document.getElementById('prev').onclick = function(){
+    let lists = document.querySelectorAll('.item');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+  
