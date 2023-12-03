6ca04dd160c9e0ede8b31e917691d3a73e69e235
dangkhoa2004 <149858139+dangkhoa2004@users.noreply.github.com>
2023-12-03 09:54:59 +0700

    notthing

--- script.js
+++ script.js
@@ -36,4 +36,4 @@ document.getElementById('prev').onclick = function(){
     let lists = document.querySelectorAll('.item');
     document.getElementById('slide').prepend(lists[lists.length - 1]);
 }
-  
+
